30bd6ac1a11de0bf6ff25ef40752023e9204539a
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-24 09:55:49 -0600

    Create More Components & Initial Setup

---
+++ src/CryptoDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CryptoDetail() {
+  return <div>CryptoDetail</div>;
+}
+
+export default CryptoDetail;
---
+++ src/Header.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import logo from "./images/react.png";
+
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark   ">
+        <div className="container-fluid">
+          <img
+            src={logo}
+            alt=""
+            style={{ height: "35px", verticalAlign: "top" }}
+          />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
