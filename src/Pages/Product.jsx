eff2efeb09b3b7fd78c2e913d053893f7775cb1e
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-11-24 11:55:39 -0600

    Another Navigation Method

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,12 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, Navigate } from "react-router-dom";
+import { useState } from "react";
 import React from "react";
 
 function Product() {
   const navigate = useNavigate();
+  const [goToProduct, setGoToProduct] = useState(() => {
+    return false;
+  });
 
   return (
     <div>
@@ -17,6 +21,14 @@ function Product() {
       <Link to={`/product/details/5`}>
         <button>Navigate to Product Detail - 5</button>
       </Link>
+      {goToProduct && <Navigate to="/product/details/3" />}
+      <button
+        onClick={() => {
+          setGoToProduct({ goToProduct: true });
+        }}
+      >
+        Navigate to Product -3 (UseState)
+      </button>
     </div>
   );
 }
